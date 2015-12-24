2d8554b59b8ad70870b1274b99d6b64193205b41
Tang Ziya <tcztzy@gmail.com>
2015-12-24 09:22:57 -0500

    add fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,23 @@
-from django.db import models
+# coding: utf-8
+from __future__ import unicode_literals
+
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Account(models.Model):
     id = models.IntegerField(auto_created=True, primary_key=True)
-    user = models.ForeignKey(User)
+    user = models.OneToOneFielde(User)
     nickname = models.CharField(max_length=20)
     avatar = models.ImageField(upload_to='avatar')
-    
+    bdc_quota = models.IntegerField(default=50)
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, **kwargs):
+        if self not in Account.objects.all():
+            self.id = self.user.id
+        super(Account, self).save(**kwargs)
