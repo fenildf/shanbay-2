712e1e6bce2d8c0e5d3cde620d16e1818eb53eee
Tang Ziya <tcztzy@gmail.com>
2016-01-15 23:32:57 +0800

    add test

--- bdc/tests.py
+++ bdc/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
+
 from .views import IndexView
 
 
@@ -8,3 +10,9 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, IndexView.as_view())
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = IndexView.as_view(request)
+        self.assertTrue(response.content.startwith(b'<html>'))
+        self.assertIn(b'<title>扇贝</title>', response.content)
+        self.assertTrue(response.content.endwith(b'</html>'))
