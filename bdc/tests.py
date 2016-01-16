8906f4ed7e98cd0ea2f3d912766fe714304d320a
Tang Ziya <tcztzy@gmail.com>
2016-01-16 23:27:05 +0800

    Amend some problems in tests

--- bdc/tests.py
+++ bdc/tests.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase
@@ -10,9 +11,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, IndexView.as_view())
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = IndexView.as_view(request)
         self.assertTrue(response.content.startwith(b'<html>'))
-        self.assertIn(b'<title>扇贝</title>', response.content)
+        self.assertIn('<title>扇贝</title>'.encode('utf-8'), response.content)
         self.assertTrue(response.content.endwith(b'</html>'))
